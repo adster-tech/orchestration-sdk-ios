78da14a5f3514b5e8ac72654cf59d6fff90555d3
Akshat Goyal <53331280+akshatgoyal276@users.noreply.github.com>
2025-03-31 14:06:53 +0530

    changes

---
+++ package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Adster",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "Adster",
+            targets: ["Adster"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/realm-swift.git", from: "20.0.1")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AdsFramework",
+            path: "Frameworks/AdsFramework.xcframework"
+        ),
+        .binaryTarget(
+            name: "DTBiOSSDK",
+            path: "Frameworks/DTBiOSSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "FBAudienceNetwork",
+            path: "Frameworks/FBAudienceNetwork.xcframework"
+        ),
+        .target(
+            name: "Adster",
+            dependencies: [
+                "AdsFramework",
+                "DTBiOSSDK",
+                "FBAudienceNetwork",
+                .product(name: "RealmSwift", package: "realm-swift")
+            ],
+            path: "Sources",
+            linkerSettings: [
+                .linkedFramework("AdSupport"),
+                .linkedFramework("AppTrackingTransparency"),
+                .linkedFramework("WebKit")
+            ]
+        ),
+    ]
+)
+
