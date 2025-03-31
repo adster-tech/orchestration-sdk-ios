292b76be1d27c5442364fbc07d1030f4916d2259
Akshat Goyal <53331280+akshatgoyal276@users.noreply.github.com>
2025-03-31 14:30:07 +0530

    changes

---
+++ Sources/Adster/adster.swift
@@ -0,0 +1,12 @@
+//
+//  Adster.swift
+//  Adster
+//
+//  Created by Adster on 31/03/25.
+//
+
+public struct Adster {
+    public static func version() -> String {
+        return "1.0.8"
+    }
+}
