06df45df24e215aff81426b42c2cad31a822d47d
Adster <access@adster.tech>
2026-07-06 17:12:46 +0530

    Declare Google Mobile Ads SDK 13.2 dependency; raise min iOS to 15

--- Adster.podspec
+++ Adster.podspec
@@ -5,7 +5,7 @@ s.summary           = 'Adster Sdk provides you complete ads support for your iOS
 s.homepage          = 'https://adster.tech/'
 s.license      = "MIT"
 s.authors      = { "Adster" => "" }
-s.platform     = :ios, "13.0"
+s.platform     = :ios, "15.0"
 s.swift_version = "5.0"
 
 s.source            = {
@@ -16,4 +16,9 @@ s.source            = {
     'Frameworks/AdsFramework.xcframework',
   ]
 s.frameworks = 'AdSupport', 'AppTrackingTransparency', 'WebKit'
+
+# AdsFramework.xcframework is built against the Google Mobile Ads SDK
+# (13.2.0). It must be declared so consumers get the GoogleMobileAds module
+# transitively.
+s.dependency 'Google-Mobile-Ads-SDK', '~> 13.2'
 end
--- Package.swift
+++ Package.swift
@@ -4,13 +4,22 @@ import PackageDescription
 let package = Package(
     name: "Adster",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         .library(
             name: "Adster",
             type: .static,
             targets: ["Adster"]
+        ),
+    ],
+    dependencies: [
+        // AdsFramework.xcframework is built against Google Mobile Ads SDK
+        // 13.2.0. Declaring it here makes the GoogleMobileAds module available
+        // to consumers of the Adster library.
+        .package(
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            .upToNextMajor(from: "13.2.0")
         ),
     ],
     targets: [
@@ -21,7 +30,11 @@ let package = Package(
         .target(
             name: "Adster",
             dependencies: [
-                "AdsFramework"
+                "AdsFramework",
+                .product(
+                    name: "GoogleMobileAds",
+                    package: "swift-package-manager-google-mobile-ads"
+                )
             ],
             path: "Sources",
             linkerSettings: [
